5a7324a98a17bbae876fb5eb48cdd676b217cdd0
aspire864 <aspire864@aspire864.Aspiresys.com>
2015-10-19 10:22:57 +0530

    17-oct-15

--- Gangouts/Tasks/model/database_queries.php
+++ Gangouts/Tasks/model/database_queries.php
@@ -26,6 +26,31 @@ class database
          return $query;
      }
 	
+     public function select()
+     {
+     	$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+     	 
+     
+     	$sql = 'SELECT id, user_name, blood_group FROM user_details';
+     	// mysqli_select_db('Gangouts');
+     	$retval = mysqli_query($conn , $sql );
+     	 
+     	if(! $retval ) {
+     		die('Could not get data: ' );
+     	}
+     	while($row = mysqli_fetch_assoc($retval)) {
+     		echo    "USER ID :{$row['id']}  <br> ".
+     				"USER NAME : {$row['user_name']} <br> ".
+     				" Blood Group : {$row['blood_group']} <br> ".
+     				"--------------------------------<br>";
+     	}
+     	 
+     	echo "You are successfully Registered\n";
+     	 
+     	mysqli_close($conn);
+     
+     
+     }
 
 
 }
